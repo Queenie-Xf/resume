a9693ebcd530474e10fc43cc809600b919da0d4b
Queenie-Xf <fxqueenie07@gmail.com>
2026-08-03 19:46:01 +0800

    Simplify entry point for static deployments

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
-const baseUrl = import.meta.env.BASE_URL
-const routerBasename = baseUrl === '/' ? undefined : baseUrl.replace(/\/$/, '')
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={routerBasename}>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
